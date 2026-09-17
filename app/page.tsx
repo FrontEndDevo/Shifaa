2bad950fb0779f57d780ea02f9ed784a799ab81b
FrontEndDevo <mylinkacco@gmail.com>
2026-09-17 15:48:38 +0300

    Add Home component with PatientForm and layout structure

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex max-h-screen">
+      {/* Todo: OTP verification | passkey*/}
+
+      <section className="container remove-scrollbar my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-gray-600 xl:text-left">
+              © 2026 Shifaa
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+}
