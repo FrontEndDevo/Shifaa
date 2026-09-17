0e9510ef058e094ad88659b555509439f1a3f0e3
FrontEndDevo <mylinkacco@gmail.com>
2026-09-17 15:52:45 +0300

    Add PatientFormDefaultValues constant for default form data

---
+++ constants/PatientFormDefaultValues.ts
@@ -0,0 +1,26 @@
+import { Gender } from "@/types";
+
+export const PatientFormDefaultValues = {
+  name: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
+  gender: "Male" as Gender,
+  address: "",
+  occupation: "",
+  emergencyContactName: "",
+  emergencyContactNumber: "",
+  primaryPhysician: "",
+  insuranceProvider: "",
+  insurancePolicyNumber: "",
+  allergies: "",
+  currentMedication: "",
+  familyMedicalHistory: "",
+  pastMedicalHistory: "",
+  identificationType: "Birth Certificate",
+  identificationNumber: "",
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
