d824543b281d424e64d17f0d48fcb26d3ddc946d
FrontEndDevo <mylinkacco@gmail.com>
2026-09-18 01:01:46 +0300

    Replace logo with Shifaa component in Register page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,6 +3,9 @@ import { getUser } from "@/lib/actions/patient.actions";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 
+// Shifaa Icon:
+import Shifaa from "@/components/layout/Shifaa";
+
 const Register = async ({ params }: SearchParamProps) => {
   const { userId } = await params;
 
@@ -12,13 +15,7 @@ const Register = async ({ params }: SearchParamProps) => {
     <div className="h-screen flex max-h-screen">
       <section className="container remove-scrollbar">
         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            alt="patient"
-            width={1000}
-            height={1000}
-            className="mb-12 h-10 w-fit"
-          />
+          <Shifaa />
 
           <RegisterForm userInfo={userInfo} />
 
