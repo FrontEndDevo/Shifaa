6f29ad030ba8973fe833ac740a3d017832ba99b7
FrontEndDevo <mylinkacco@gmail.com>
2026-09-17 15:46:47 +0300

    Add user-related interfaces and types for registration and search parameters

---
+++ types/index.ts
@@ -0,0 +1,37 @@
+export interface ICreateUserParams {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export type Gender = "Male" | "Female" | "Other";
+
+export interface IUser extends ICreateUserParams {
+  $id: string;
+}
+
+export interface IRegisterUserParams extends ICreateUserParams {
+  userId: string;
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  occupation: string;
+  emergencyContactName: string;
+  emergencyContactNumber: string;
+  primaryPhysician: string;
+  insuranceProvider: string;
+  insurancePolicyNumber: string;
+  allergies: string | undefined;
+  currentMedication: string | undefined;
+  familyMedicalHistory: string | undefined;
+  pastMedicalHistory: string | undefined;
+  identificationType: string | undefined;
+  identificationNumber: string | undefined;
+  identificationDocument: FormData | undefined;
+  privacyConsent: boolean;
+}
+
+export type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
