412e0204d8dbf24184b41c5f13c003e831bf5ff2
FrontEndDevo <mylinkacco@gmail.com>
2026-09-20 17:18:30 +0300

    Refactor Shifaa component layout and adjust text/image sizes

--- components/layout/Shifaa.tsx
+++ components/layout/Shifaa.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 const Shifaa = () => {
   return (
-    <div className="flex gap-2 mb-10 items-end">
+    <div className="flex gap-2 items-end">
       <Image
         src="/assets/icons/shifaa-icon.png"
         alt="shifaa"
         width={1000}
         height={1000}
-        className="h-16 w-16"
+        className="h-12 w-12"
       />
-      <p className="text-4xl font-bold font-mono">Shifaa</p>
+      <p className="text-3xl font-bold font-mono">Shifaa</p>
     </div>
   );
 };
