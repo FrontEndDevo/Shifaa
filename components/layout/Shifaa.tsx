9a7acdf177eae1e4bb127c308b97eec19c2d0870
FrontEndDevo <mylinkacco@gmail.com>
2026-09-20 17:27:05 +0300

    Wrap Shifaa component content in Link for improved navigation

--- components/layout/Shifaa.tsx
+++ components/layout/Shifaa.tsx
@@ -1,17 +1,20 @@
+import Link from "next/link";
 import Image from "next/image";
 
 const Shifaa = () => {
   return (
-    <div className="flex gap-2 items-end">
-      <Image
-        src="/assets/icons/shifaa-icon.png"
-        alt="shifaa"
-        width={1000}
-        height={1000}
-        className="h-12 w-12"
-      />
-      <p className="text-3xl font-bold font-mono">Shifaa</p>
-    </div>
+    <Link href="/">
+      <div className="flex gap-2 items-end">
+        <Image
+          src="/assets/icons/shifaa-icon.png"
+          alt="shifaa"
+          width={1000}
+          height={1000}
+          className="h-12 w-12"
+        />
+        <p className="text-3xl font-bold font-mono">Shifaa</p>
+      </div>
+    </Link>
   );
 };
 
