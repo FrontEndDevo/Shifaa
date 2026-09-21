ffbf668df9609c86d4eb4a8c1f44a47432677faf
FrontEndDevo <mylinkacco@gmail.com>
2026-09-21 15:09:55 +0300

    Add DataTable and AdminFeatures components with filtering and pagination functionalities

---
+++ components/admin/DataTable.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  useTable,
+  type ColumnDef,
+  type RowData,
+  type ColumnFiltersState,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { features, DataTableFeatures } from "./AdminFeatures";
+import { Input } from "@/components/ui/input";
+
+interface DataTableProps<TData extends RowData> {
+  columns: ColumnDef<DataTableFeatures, TData>[];
+  data: TData[];
+}
+
+export function DataTable<TData extends RowData>({
+  columns,
+  data,
+}: DataTableProps<TData>) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+
+  const table = useTable({
+    features,
+    data,
+    columns,
+    onColumnFiltersChange: setColumnFilters,
+    state: {
+      columnFilters,
+    },
+  });
+
+  return (
+    <>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter emails..."
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : (
+                        <table.FlexRender header={header} />
+                      )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      <table.FlexRender cell={cell} />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No appointments.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+
+        {/* Next / Previous buttons */}
+        <div className="flex items-center justify-between px-4 space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/admin/columns.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { createColumnHelper } from "@tanstack/react-table";
+
+import { DataTableFeatures } from "./AdminFeatures";
+
+import StatusBadge from "./StatusBadge";
+import { formatDateTime } from "@/lib/utils";
+import { Appointment } from "@/types/appointment.types";
+import Image from "next/image";
+import { Doctors } from "@/constants";
+import AppointmentModal from "../modals/AppointmentModal";
+
+// Use `accessor` for data columns and `display` for columns without one.
+const columnHelper = createColumnHelper<DataTableFeatures, Appointment>();
+
+export const columns = columnHelper.columns([
+  {
+    header: "ID",
+    cell: ({ row }) => <p className="text-sm">{row.index + 1}</p>,
+  },
+
+  {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => <h3 className="text-sm">{row.original.patient.name}</h3>,
+  },
+
+  {
+    accessorKey: "phone",
+    header: "Phone",
+    cell: ({ row }) => (
+      <h3 className="text-sm">{row.original.patient.phone}</h3>
+    ),
+  },
+
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div>
+        <StatusBadge status={row.original.status} />
+      </div>
+    ),
+  },
+
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-sm min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    ),
+  },
+
+  {
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
+    cell: ({ row }) => {
+      const appointment = row.original;
+
+      const doctor = Doctors.find(
+        (doctor) => doctor.name === appointment.primaryPhysician,
+      );
+
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image as string}
+            alt="doctor"
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row: { original: data } }) => {
+      return (
+        <div className="flex gap-1">
+          <AppointmentModal
+            type="schedule"
+            appointment={data}
+            title="Schedule Appointment"
+            description="Please confirm the following details to"
+          />
+
+          <AppointmentModal
+            type="cancel"
+            appointment={data}
+            title="Cancle Appointment"
+            description="Are you sure you want to cancle this appointment?"
+          />
+        </div>
+      );
+    },
+  },
+]);
