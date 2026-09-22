91cd537c9f67e7b487a3ab1069c6e09fabbe1b46
FrontEndDevo <mylinkacco@gmail.com>
2026-09-22 19:56:04 +0300

    Add loading components for new appointment, success, and register pages

---
+++ app/patients/[userId]/register/loading.tsx
@@ -0,0 +1,9 @@
+import SpinnerButton from "@/components/shared/SpinnerButton";
+
+export default function RegisterLoading() {
+  return (
+    <div className="flex justify-center items-center size-full h-screen w-full">
+      <SpinnerButton />
+    </div>
+  );
+}
