0cbc45c62aa9be46b63773f4b08575415c98df09
FrontEndDevo <mylinkacco@gmail.com>
2026-09-23 14:03:35 +0300

    Set dynamic rendering and disable revalidation for admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 // Data Table:
 import { DataTable } from "@/components/admin/DataTable";
 import { columns } from "@/components/admin/columns";
